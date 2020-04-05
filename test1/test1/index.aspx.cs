55a088c9b2f1aed1510e07cb31d04555c0be322c
sachin <17ceuog044@gmail.com>
2020-04-06 02:35:49 +0530

    db added

---
+++ test1/test1/index.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using test1.ServiceReference1;
+
+namespace test1
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        ServiceReference1.Service1Client se = new ServiceReference1.Service1Client();
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Details d = new Details();
+            d.LotArea =float.Parse(t1.Text);
+            d.Street = DropDownList1.SelectedValue;
+            d.Utilities = DropDownList2.SelectedValue;
+            d.Neighborhood = DropDownList3.SelectedValue;
+            d.HouseStyle = DropDownList4.SelectedValue;
+            d.OverallQual = float.Parse(t2.Text);
+            d.OverallCond = float.Parse(t3.Text);
+            d.YearBuilt = float.Parse(t4.Text);
+            d.TotalBsmtSF = float.Parse(t5.Text);
+            d.CentralAir = bool.Parse(t6.Text);
+            d.FirstFlrSF = float.Parse(t7.Text);
+            d.SecondFlrSF = float.Parse(t8.Text);
+            d.GrLivArea = float.Parse(t9.Text);
+            d.FullBath = float.Parse(t10.Text);
+            d.BedroomAbvGr = float.Parse(t11.Text);
+            d.KitchenAbvGr = float.Parse(t12.Text);
+            d.SellPrice = float.Parse(t13.Text);
+            d.TotRmsAbvGrd = float.Parse(t14.Text);
+            float re = se.predict_value(d);
+            Label1.Text = "Predicted value"+re.ToString();
+
+
+        }
+    }
+}
---
+++ test1_service/test1_service/Service1.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Test1_serviceML.Model;
+
+namespace test1_service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public float predict_value(Details d)
+        {
+            ModelInput ml = new ModelInput();
+            ml.LotArea = d.LotArea;
+            ml.Street = d.Street;
+            ml.Neighborhood = d.Neighborhood;
+            ml.Utilities = d.Utilities;
+            ml.HouseStyle = d.HouseStyle;
+            ml.OverallCond = d.OverallCond;
+            ml.OverallQual = d.OverallQual;
+            ml.YearBuilt = d.YearBuilt;
+            ml.TotalBsmtSF = d.TotalBsmtSF;
+            ml.CentralAir = d.CentralAir;
+            ml.Col1stFlrSF = d.FirstFlrSF;
+            ml.Col2ndFlrSF = d.SecondFlrSF;
+            ml.GrLivArea = d.GrLivArea;
+            ml.BedroomAbvGr = d.BedroomAbvGr;
+            ml.FullBath = d.FullBath;
+            ml.KitchenAbvGr = d.KitchenAbvGr;
+            ml.TotRmsAbvGrd = d.TotRmsAbvGrd;
+            ml.SalePrice = d.SellPrice;
+            ConsumeModel c = new ConsumeModel();
+     
+            var p= ConsumeModel.Predict(ml);
+            var re = p.Score;
+            return re;
+            
+
+        }
+    }
+}
